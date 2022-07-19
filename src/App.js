ef7cea6513ef2d140cda8244b63dac50e4e97acd
Abdul Majid <abdulmajidcse@gmail.com>
2022-07-19 18:28:35 +0600

    react-reduxt practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+} from "./features/counter/counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Couter Value: <code>{count}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount(5))}
         >
-          Learn React
-        </a>
+          Increment By 5
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrementByAmount(5))}
+        >
+          Decrement By 5
+        </button>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
       </header>
     </div>
   );
