3601943b3419c8574c3d420f4af549da4db2bbf4
Abdul Majid <abdulmajidcse@gmail.com>
2022-08-21 00:07:32 +0600

    add react boostrap and setup.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import {
   increment,
@@ -7,44 +5,37 @@ import {
   incrementByAmount,
   decrementByAmount,
 } from "./features/counter/counterSlice";
+import { Button, ButtonGroup } from "react-bootstrap";
 
 function App() {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <>
+      <div className="container mt-3">
+        <h4>
           Couter Value: <code>{count}</code>
-        </p>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(incrementByAmount(5))}
-        >
-          Increment By 5
-        </button>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrementByAmount(5))}
-        >
-          Decrement By 5
-        </button>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </header>
-    </div>
+        </h4>
+        <ButtonGroup aria-label="Counter Buttons">
+          <Button variant="info" onClick={() => dispatch(incrementByAmount(5))}>
+            +5
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => dispatch(decrementByAmount(5))}
+          >
+            -5
+          </Button>
+          <Button variant="success" onClick={() => dispatch(increment())}>
+            +1
+          </Button>
+          <Button variant="danger" onClick={() => dispatch(decrement())}>
+            -1
+          </Button>
+        </ButtonGroup>
+      </div>
+    </>
   );
 }
 
